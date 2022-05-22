312278c5a986897e7c9e36b04e0e28e685398d0c
Hyunwoo Choi <62496215+gusdn7142@users.noreply.github.com>
2022-05-22 12:33:13 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,14 @@
+T = int(input()) 
+
+cnt = T  
+for i in range(T):  
+    word = input() 
+
+    for j in range(0, len(word)-1):
+        if word[j] == word[j+1]:  
+           continue 
+        elif word[j] in word[j+1:]: 
+            cnt -= 1
+            break
+
+print(cnt)
