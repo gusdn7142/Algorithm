02d05f8556c6cc812f4bdda5ae229d7a7843988b
gusdn7142 <gusdnchl7144@naver.com>
2022-05-26 15:32:40 +0900

    2557번 Hello World.java

--- BOJ_Java/Hello_World.java
+++ BOJ_Java/Hello_World.java
@@ -4,7 +4,7 @@
 
 
 
-public class Main {
+public class Hello_World {
 
     public static void main(String[] args) {
         System.out.println("강한친구 대한육군");
---
+++ 백준/Bronze/2557. Hello World/Hello_World.java
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+public class Hello_World {
+
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+
+    }
+}
