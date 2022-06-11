1bc105aa1214b424624f2fe2b0c8d65d98d2d1e0
Hyunwoo Choi <62496215+gusdn7142@users.noreply.github.com>
2022-06-11 18:14:30 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 212 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        //정수 n 입력
+        int n = scan.nextInt();
+
+        //d[1001] 배열 생성
+        int d[] = new int[1001];  //n의 최대 크기는 1000이기 때문에 1001
+
+        d[1] = 1;  //2*1의 직사각형을 채우는 방법 : 1가지
+        d[2] = 3;  //2*2의 직사각형을 채우는 방법 : 3가지
+
+        for(int i=3; i<=n; i++) {   //3부터 n까지 반복
+
+            d[i] = d[i-1] + 2*d[i-2];   //점화식 적용
+            d[i] %= 10007;
+        }
+
+        System.out.println(d[n]);
+
+    }
+
+}
