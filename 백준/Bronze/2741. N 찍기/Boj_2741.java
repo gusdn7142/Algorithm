131d194f3750794e1bf40fc29834186cc0038ffd
gusdn7142 <gusdnchl7144@naver.com>
2022-07-19 19:33:13 +0900

    [Bronze III] Title: N 찍기 (Java)

---
+++ 백준/Bronze/2741. N 찍기/Boj_2741.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Boj_2741 {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        for(int i=0; i<n; i++){
+            System.out.println(i+1);
+        }
+
+
+    }
+
+}
