708f0dbdabd61f7d6ae9af0ab4441e4a108ebd8e
gusdn7142 <gusdnchl7144@naver.com>
2022-10-01 07:52:49 +0900

    삼각형 출력 3
    반복문 활용

---
+++ AlgorithmJobs_Java/Part_2_다중_반목문and배열/삼각형_출력_3.java
@@ -0,0 +1,26 @@
+package Part_2_다중_반목문and배열;
+
+import java.util.Scanner;
+
+public class 삼각형_출력_3 {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        //n개의 행 출력
+        for(int i=0; i<n; i++){
+
+            //공백 출력
+            for(int j=0; j<n-1-i; j++){
+                System.out.print(" ");
+            }
+            //"*" 출력
+            for(int j=0; j<2*i+1; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();  //줄바꿈
+        }
+    }
+}
