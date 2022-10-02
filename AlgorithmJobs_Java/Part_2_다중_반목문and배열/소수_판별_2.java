35d6178dd0ad640c7e261cd820c5165648e64576
gusdn7142 <gusdnchl7144@naver.com>
2022-10-02 19:13:33 +0900

    소수 판별 2
    반복문과 조건문 활용

---
+++ AlgorithmJobs_Java/Part_2_다중_반목문and배열/소수_판별_2.java
@@ -0,0 +1,38 @@
+package Part_2_다중_반목문and배열;
+
+import java.util.Scanner;
+
+public class 소수_판별_2 {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        //자연수 n과 m 입력
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+
+
+        //n부터 m까지 반복
+        for(int i=n; i<=m; i++){
+
+            //카운트 변수 초기화
+            int cnt = 0;
+
+            if(i==1){     //i가 1이면 소수에서 제외
+                cnt=1;
+            }
+
+            //i가 소수인지 판별
+            for(int j=2; j<=i-1; j++){
+                if(i%j==0){   //i가 소수가 아니면
+                    cnt=1;
+                    break;
+                }
+            }
+
+            if(cnt==0){   //i가 소수이면 i 출력
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
