7e3d3b449fa070bd76523ff902518cff2adecb62
gusdn7142 <gusdnchl7144@naver.com>
2022-10-20 18:02:38 +0900

    과제물 망치기
    문자 배열, 반복문, 조건문 활용

---
+++ AlgorithmJobs_Java/Part_4_문자열/과제물_망치기.java
@@ -0,0 +1,24 @@
+package Part_4_문자열;
+
+import java.util.Scanner;
+
+public class 과제물_망치기 {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        //입력
+        String str = scan.nextLine();    //문자열 입력
+
+        //변수 초기화
+        char[] ch = str.toCharArray();   //문자열-> 문자배열로 변환
+
+        //문자 배열에서 공백(' ')를 제외하고 출력
+        for(int i=0; i<ch.length; i++){
+            if(ch[i] != ' '){            //문자배열의 요소가 공백(' ')이 아니라면
+                System.out.print(ch[i]);
+            }
+        }
+
+    }
+}
