c05c903615d0a6844d8c22e28c51924384f2960a
gusdn7142 <gusdnchl7144@naver.com>
2022-11-24 16:38:35 +0900

    깊이우선 탐색과 너비우선 탐색
    배열, 리스트, 큐, 조건문, 반복문 활용

---
+++ AlgorithmJobs_Java/Part_10_BFS/깊이우선_탐색과_너비우선_탐색.java
@@ -0,0 +1,96 @@
+package Part_10_BFS;
+
+import java.util.*;
+
+public class 깊이우선_탐색과_너비우선_탐색 {
+
+    static int MAX = 1005;
+    static int n; //정점의 개수
+    static int m; //간선의 개수
+    static List<ArrayList<Integer>> graph = new ArrayList<>();  //그래프 선언
+    static boolean visitedDFS[] = new boolean[MAX];   //DFS 순회시 정점 방문여부 체크
+    static Queue<Integer> queue = new LinkedList<>();
+    static boolean visitedBFS[] = new boolean[MAX];   //BFS 순회시 정점 방문여부 체크
+
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        //입력
+        n = scan.nextInt();  //정점의 개수
+        m = scan.nextInt();  //간선의 개수
+
+        //정점 입력 및 인접리스트 연결
+
+        for(int i=0; i<n; i++){
+            graph.add(new ArrayList<Integer>());
+        }
+
+        for(int i=0; i<m; i++){
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+
+            graph.get(a).add(b);  //정점 연결
+            graph.get(b).add(a);  //정점 연결
+        }
+
+        for(int i=0; i<n; i++){
+            Collections.sort(graph.get(i));      //노드 번호가 작은 순서대로 방문하기 때문에 그래프 정렬 필요
+        }
+
+        // for(int i=0; i<n; i++){
+        //     System.out.println(graph.get(i));
+        // }
+
+        DFS(0);  //노드 0번부터 DFS로 그래프 순회 시작
+        System.out.println();
+        BFS();   //노드 0번부터 BFS로 그래프 순회 시작
+
+    }
+
+
+    static void DFS(int x){
+        //노드 0번부터 DFS로 그래프를 순회하는 깊이우선탐색 재귀함수
+
+        visitedDFS[x] = true;  //x번 정점 방문
+        System.out.print(x + " ");  //정점 x 출력
+
+        for(int i=0; i<graph.get(x).size(); i++){
+
+            int y = graph.get(x).get(i);   //x의 연결 요소 조회
+
+            if(visitedDFS[y]==false){   //y정점을 방문하지 않았다면
+                DFS(y);                 //y정점에 대한 DFS 그래프 순회
+            }
+        }
+    }
+
+
+    static void BFS(){
+
+        queue.add(0);         //0번 노드를 큐에 삽입
+        visitedBFS[0] = true;  //0번 노드 방문 체크
+
+        while(queue.size() > 0){  //큐의 크기가 0이 아니면 계속 반복
+
+            int current = queue.peek();  //큐의 맨앞에 값 반환
+            queue.remove();             //큐의 맨 앞값 삭제
+            System.out.print(current + " ");       //큐의 맨 앞값 출력
+
+            for(int i=0; i<graph.get(current ).size(); i++){
+
+                int next = graph.get(current ).get(i);  //연결된 요소 반환
+
+                if(visitedBFS[next]==false){   //next 정점에 방문하지 않았다면
+                    visitedBFS[next]=true;   //next 정점에 방문
+                    queue.add(next);        //큐에 next 정점 삽입
+                }
+            }
+        }
+
+
+    }
+
+
+}
