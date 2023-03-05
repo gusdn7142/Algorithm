1460766b94214c03068058eb79aa6a97f5a3ee54
gusdn7142 <gusdnchl7144@naver.com>
2023-03-05 11:37:11 +0900

    배열의 평균값
    - 배열의_평균값_v1 : '+'와‘/'연산자 사용
    - 배열의_평균값_v2 : Arrays.stream().average() 사용

---
+++ programmers/src/Lv0/배열의_평균값_v2.java
@@ -0,0 +1,30 @@
+package Lv0;
+
+import java.util.Arrays;
+
+public class 배열의_평균값_v2 {
+    public double solution(int[] numbers) {
+        double result = 0;
+
+        boolean bool1 = true;
+        boolean bool2 = true;
+
+        //numbers 배열의 모든 요소가 0과 1000사이에 있는 값이지 체크
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i]<0 && 1000<numbers[i]){
+                bool1 = false;
+                break;
+            }
+        }
+
+        //numbers 배열의 길이가 1과 100사이의 값인지 체크
+        bool2 = (1<=numbers.length && numbers.length<=100) ? true : false;
+
+        //위의 조건들을 만족한다면, numbers 배열의 평균값 계산
+        if(bool1 && bool2){
+            result = Arrays.stream(numbers).average().orElse(-1);
+        }
+
+        return result;
+    }
+}
