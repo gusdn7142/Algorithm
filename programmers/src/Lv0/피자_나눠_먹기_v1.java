fecdc3e875efa1db888a5a1d6a3edc62a59acc49
gusdn7142 <gusdnchl7144@naver.com>
2023-04-02 13:18:40 +0900

    피자 나눠 먹기1
    '%'와 '/' 연산자 사용

---
+++ programmers/src/Lv0/피자_나눠_먹기_v1.java
@@ -0,0 +1,14 @@
+package Lv0;
+
+public class 피자_나눠_먹기_v1 {
+    public int solution(int n) {
+        int result = -1;
+
+        if(1<=n && n<=100){   //n값의 범위 체크
+            if(n % 7 == 0) result = n / 7;  //필요한 피자 수 계산
+            else result = (n / 7) + 1;
+        }
+
+        return result;
+    }
+}
