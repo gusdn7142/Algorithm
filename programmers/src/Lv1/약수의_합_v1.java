55ff20bbcac406e9de366844077784a477d0f143
gusdn7142 <gusdnchl7144@naver.com>
2023-05-17 12:42:56 +0900

    약수의 합
    - 약수의_합_v1 : 반복문과 조건문 사용
    - 약수의_합_v2 : Stream 사용

---
+++ programmers/src/Lv1/약수의_합_v1.java
@@ -0,0 +1,16 @@
+package Lv1;
+
+public class 약수의_합_v1 {
+    public int solution(int n) {
+        int result = 0;
+
+        //n의 약수의 합 계산
+        for(int i=1; i<=n/2; i++){
+            if(n % i ==0) {
+                result += i;
+            }
+        }
+
+        return result+n;
+    }
+}
---
+++ programmers/src/Lv1/약수의_합_v2.java
@@ -0,0 +1,15 @@
+package Lv1;
+
+import java.util.stream.IntStream;
+
+public class 약수의_합_v2 {
+    public int solution(int n) {
+        int result = 0;
+
+        result = IntStream.rangeClosed(1, n)
+                .filter(i -> n % i ==0)
+                .sum();
+
+        return result;
+    }
+}
