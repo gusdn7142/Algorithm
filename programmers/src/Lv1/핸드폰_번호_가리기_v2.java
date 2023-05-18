aed4c0c91f2f865aac3c84d707979d8f589bb864
gusdn7142 <gusdnchl7144@naver.com>
2023-05-18 10:52:53 +0900

    핸드폰 번호 가리기
    - 핸드폰_번호_가리기_v1 : 반복문과 조건문 사용
    - 핸드폰_번호_가리기_v2 : String의 substring() 메서드 사용

---
+++ programmers/src/Lv1/핸드폰_번호_가리기_v2.java
@@ -0,0 +1,17 @@
+package Lv1;
+
+public class 핸드폰_번호_가리기_v2 {
+    public String solution(String phone_number) {
+
+        StringBuilder sb = new StringBuilder();
+        int length = phone_number.length();
+
+        for(int i=0; i<length-4; i++){
+            sb.append("*");
+        }
+
+        sb.append(phone_number.substring(length-4));
+
+        return sb.toString();
+    }
+}
