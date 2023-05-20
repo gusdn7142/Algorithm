9cec95fe44a199957ec8ae2ad0e942727747f28d
gusdn7142 <gusdnchl7144@naver.com>
2023-05-20 20:12:20 +0900

    소수 찾기
    반복문과 Math.sqrt() 메서드 사용

---
+++ programmers/src/Lv1/소수_찾기_v1.java
@@ -0,0 +1,24 @@
+package Lv1;
+
+public class 소수_찾기_v1 {
+    public int solution(int n) {
+        int count = 0;
+
+        //1부터 n까지의 소수의 개수 계산
+        for(int i=2; i<=n; i++){
+            boolean flag = true;    //소수 판별
+
+            for(int j=2; j<=Math.sqrt(i); j++){     //i의 제곱근까지 반복
+                if(i % j ==0){
+                    flag = false;   //소수가 아님
+                    break;
+                }
+            }
+
+            if(flag)  //소수이면
+                count++;
+        }
+
+        return count;
+    }
+}
