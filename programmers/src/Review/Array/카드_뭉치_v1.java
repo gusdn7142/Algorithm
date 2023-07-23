7afdad4fcbae759eab035a0813e3a9a5a4010d0b
gusdn7142 <gusdnchl7144@naver.com>
2023-07-23 13:50:43 +0900

    카드 뭉치
    반복문, 조건문 사용

---
+++ programmers/src/Review/Array/카드_뭉치_v1.java
@@ -0,0 +1,24 @@
+package Review.Array;
+
+public class 카드_뭉치_v1 {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String result = "Yes";
+        int cardIdx1 = 0;
+        int cardIdx2 = 0;
+
+        //goal[] 배열의 요소가 card1[]와 card2[] 배열에 존재하는지 검사
+        for(int i=0; i<goal.length; i++){
+
+            if(cardIdx1 < cards1.length && goal[i].equals(cards1[cardIdx1]) ){   //순서 중요
+                cardIdx1++;
+            } else if(cardIdx2 < cards2.length && goal[i].equals(cards2[cardIdx2])) {  //순서 중요
+                cardIdx2++;
+            } else {
+                result = "No";
+                break;
+            }
+        }
+
+        return result;
+    }
+}
