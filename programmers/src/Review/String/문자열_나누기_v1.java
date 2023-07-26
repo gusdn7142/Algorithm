55f1e74c659b3636e1d856a0c93c3bcb072c58ca
gusdn7142 <gusdnchl7144@naver.com>
2023-07-26 12:39:14 +0900

    문자열 나누기
    반복문, 조건문, String 사용

---
+++ programmers/src/Review/String/문자열_나누기_v1.java
@@ -0,0 +1,31 @@
+package Review.String;
+
+public class 문자열_나누기_v1 {
+    public int solution(String str) {
+        int result = 0;   //분리한 문자열의 개수 증가
+        char x = str.charAt(0);
+
+        int count = 1;   //같은 숫자면 ++.  다른 숫자면 --
+
+        for(int i=1; i<str.length(); i++){
+            if (count == 0) {  //같은 문자와 다른 문자의 개수가 같으면
+                x = str.charAt(i);
+            }
+
+            if (x == str.charAt(i)) {    //같은 문자이면 count++
+                count++;
+            } else {                    //다른 문자이면 count--
+                count--;
+            }
+
+            if (count == 0) {  //같은 문자와 다른 문자의 개수가 같으면
+                result++;      //분리한 문자열의 개수 증가
+            }
+        }
+
+        if(count > 0)        //남는 문자가 있다면
+            result++;       //분리한 문자열의 개수 증가
+
+        return result;
+    }
+}
