902d2b90e4454a6ea5a3762193efad3cc3455fc0
gusdn7142 <gusdnchl7144@naver.com>
2023-07-27 10:23:07 +0900

    가운데 글자 가져오기
    조건문, str.substring() 메서드 사용

---
+++ programmers/src/Review/String/가운데_글자_가져오기_v1.java
@@ -0,0 +1,18 @@
+package Review.String;
+
+public class 가운데_글자_가져오기_v1 {
+    public String solution(String str) {
+
+        String result = "";
+        int length = str.length();
+
+        if(length % 2 ==0){      //str의 길이가 짝수이면
+            result = str.substring(length/2-1, length/2+1);
+        }
+        else  //str의 길이가 홀수이면
+            result = str.substring(length/2, length/2+1);
+
+
+        return result;
+    }
+}
