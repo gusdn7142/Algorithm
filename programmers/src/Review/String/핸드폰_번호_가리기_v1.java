90b0895b1bfdb53b947398805044c94392a0e918
gusdn7142 <gusdnchl7144@naver.com>
2023-08-05 00:50:59 +0900

    핸드폰 번호 가리기
    반복문과 조건문 사용

---
+++ programmers/src/Review/String/핸드폰_번호_가리기_v1.java
@@ -0,0 +1,19 @@
+package Review.String;
+
+public class 핸드폰_번호_가리기_v1 {
+    public String solution(String phone_number) {
+
+        StringBuilder sb = new StringBuilder();
+        int length = phone_number.length();
+
+        for(int i=0; i<length; i++){
+            if(i < length-4){
+                sb.append("*");
+            } else{
+                sb.append(phone_number.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+}
