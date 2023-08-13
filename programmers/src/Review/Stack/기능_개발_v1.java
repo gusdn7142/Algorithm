0605d6a88cd9df19c507ed26e640e029f4997516
gusdn7142 <gusdnchl7144@naver.com>
2023-08-14 00:20:49 +0900

    기능 개발
    Stack, 반복문, 조건문, 배열, Stream 사용

---
+++ programmers/src/Review/Stack/기능_개발_v1.java
@@ -0,0 +1,41 @@
+package Review.Stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class 기능_개발_v1 {
+    public int[] solution(int[] progresses, int[] speeds) {
+
+        Stack<Integer> stack = new Stack<>();   //기능별 배포일자 저장
+
+        //1. 기능별 배포일자를 stack에 삽입
+        for(int i=0; i<progresses.length; i++){
+            int releaseDay = 0;    //배포일 계산
+
+            while(progresses[i]<100){
+                progresses[i] += speeds[i];
+                releaseDay++;
+            }
+            if(!stack.empty() && stack.peek() > releaseDay){
+                releaseDay = stack.peek();
+            }
+            stack.push(releaseDay);  //5 10 10 10 20 20
+            //System.out.println(releaseDay);
+        }
+
+
+        //2. 배포일자별 기능 수 계산
+        int[] visited = new int[stack.peek()+1];   //배포일자별 기능 수 저장
+        for (int releaseDay : stack) {
+            visited[releaseDay]++;                 //배포일자별 기능 수 카운트
+        }
+
+        //System.out.println(Arrays.toString(visited));
+
+        int result[] = Arrays.stream(visited)   //각 배포마다 배포되는 기능 수
+                .filter(i -> i != 0)            //배포 일자별 기능 수가 0이면 제외
+                .toArray();
+
+        return result;
+    }
+}
