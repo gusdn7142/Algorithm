31b50ac16831b90cea3707147b1a0fe21b195c47
gusdn7142 <gusdnchl7144@naver.com>
2023-08-21 10:51:21 +0900

    점수 계산
    배열, 반복문 사용

---
+++ programmers/src/Review/Array/점수_계산_v1.java
@@ -0,0 +1,31 @@
+package Review.Array;
+
+import java.util.Scanner;
+
+public class 점수_계산_v1 {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        //문제의 개수 입력
+        int n = scan.nextInt();
+
+        //채점결과 입력
+        int answer[] = new int[n+1];
+        for(int i=1; i<=n; i++){
+            answer[i] = scan.nextInt();
+        }
+
+        //점수 계산
+        int score[] = new int[n+1];
+        int sum = 0;
+        for(int i=1; i<=n; i++){
+            if(answer[i]==0) score[i]=0;   //결과가 0점이면 0점저장
+            else score[i]=score[i-1]+1;    //결과가 1점이면 이전 값 + 1점 저장
+            sum += score[i];
+        }
+        //총 점 출력
+        System.out.println(sum);
+
+    }
+}
