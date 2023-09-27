edae581a357fb137be6a19bca91c32b1433fac35
gusdn7142 <gusdnchl7144@naver.com>
2023-09-27 15:50:22 +0900

    행렬의 덧셈

---
+++ programmers/src/Review/Loop/행렬의_덧셈_v1.java
@@ -0,0 +1,16 @@
+package Review.Loop;
+
+
+public class 행렬의_덧셈_v1 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] result = new int[arr1.length][arr1[0].length];
+
+        for(int row=0; row<arr1.length; row++){
+            for(int col=0; col<arr1[row].length; col++){
+                result[row][col] = arr1[row][col] + arr2[row][col];
+            }
+        }
+
+        return result;
+    }
+}
