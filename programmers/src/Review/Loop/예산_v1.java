37f167542de893e1a0fd1ead1d5b9311d9f0c7a6
gusdn7142 <gusdnchl7144@naver.com>
2023-09-29 01:07:45 +0900

    예산

---
+++ programmers/src/Review/Loop/예산_v1.java
@@ -0,0 +1,18 @@
+package Review.Loop;
+
+import java.util.Arrays;
+
+public class 예산_v1 {
+    public int solution(int[] d, int budget) {
+        int count = 0;   //최대 물품 지원 가능 부서 수
+        Arrays.sort(d);
+
+        for(int i=0; i<d.length; i++){
+            budget -= d[i];
+            if(budget < 0) break;
+            count++;
+        }
+
+        return count;
+    }
+}
