2d5e4488f97c796ea003499e958fe067d6019d3d
gusdn7142 <gusdnchl7144@naver.com>
2023-10-01 08:53:15 +0900

    과일 장수

---
+++ programmers/src/Review/Loop/과일_장수_v1.java
@@ -0,0 +1,20 @@
+package Review.Loop;
+
+import java.util.Arrays;
+
+public class 과일_장수_v1 {
+    public int solution(int k, int m, int[] score) {
+        int result = 0;
+
+        //score[] 배열 오름차순 정렬
+        Arrays.sort(score);
+
+        //m개씩 최대한 많은 사과를 파려고 할때, 최대 이익 계산
+        int cnt = 0;
+        for(int i=score.length; i>=m; i-=m){
+            result += score[i-m] * m;
+        }
+
+        return result;
+    }
+}
